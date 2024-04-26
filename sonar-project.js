985d7d45376d2eeaae8328e87fcd75459595ad82
mvenkatgit <156108418+mvenkatgit@users.noreply.github.com>
2024-04-26 20:41:33 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://100.26.250.194:9000/',
+  serverUrl: 'http://52.87.252.121:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
